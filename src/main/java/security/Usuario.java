57ec1d2811a1b1672e4fa75486d69b33faaaa671
regispires <regispiresmag@gmail.com>
2014-04-08 20:42:27 -0300

    incluídas dependências no pom.xml. Incluídos arquivos web

--- src/main/java/security/Usuario.java
+++ src/main/java/security/Usuario.java
@@ -1,21 +1,39 @@
 package security;
 
-import entities.Context;
-import entities.CurrentUser;
-import entities.Repository;
-import entities.annotations.*;
-import entities.dao.DAOConstraintException;
-import entities.dao.DAOException;
-import entities.dao.DAOValidationException;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
-import javax.persistence.*;
+
+import javax.persistence.Column;
+import javax.persistence.ElementCollection;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
 import javax.validation.constraints.NotNull;
+
 import org.hibernate.annotations.Cascade;
 import org.hibernate.annotations.CascadeType;
 import org.hibernate.annotations.Type;
 import org.hibernate.validator.constraints.NotEmpty;
+
+import entities.Context;
+import entities.Repository;
+import entities.annotations.ActionDescriptor;
+import entities.annotations.Editor;
+import entities.annotations.EntityDescriptor;
+import entities.annotations.Param;
+import entities.annotations.ParameterDescriptor;
+import entities.annotations.PropertyDescriptor;
+import entities.annotations.View;
+import entities.annotations.Views;
+import entities.dao.DAOConstraintException;
+import entities.dao.DAOException;
+import entities.dao.DAOValidationException;
+import entities.security.CurrentUser;
 
 @Entity
 @NamedQueries({
